3a59a7e45b6418a820bfbdaaa40ac1dabbabcdd4
CommandLineDesign <commandlinedesign@gmail.com>
2025-12-04 16:20:39 +0900

    Trigger Vercel deployment after rate limit

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,5 +1,6 @@
 import { startServer } from './server';
 
+// Main entry point for the API server
 startServer().catch((error) => {
   // eslint-disable-next-line no-console
   console.error('Failed to start API server', error);
