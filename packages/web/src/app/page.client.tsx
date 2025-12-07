f83cbd628e5164e1da1223635871ee1d223dfcd0
CommandLineDesign <commandlinedesign@gmail.com>
2025-12-07 12:20:23 +0900

    updated admin app styles

--- packages/web/src/app/page.client.tsx
+++ packages/web/src/app/page.client.tsx
@@ -4,6 +4,20 @@ import dynamic from 'next/dynamic';
 
 const AdminApp = dynamic(() => import('../components/AdminApp'), {
   ssr: false,
+  loading: () => (
+    <div
+      style={{
+        height: '100vh',
+        display: 'grid',
+        placeItems: 'center',
+        background: '#f5f5f5',
+        color: '#555',
+        fontFamily: 'Roboto, sans-serif',
+      }}
+    >
+      Loading admin…
+    </div>
+  ),
 });
 
 export default function Home() {
